7c535d56977217e17e6d86a334002ee02fe93ccd
grbzlyk <petr_skazko@mail.ru>
2019-12-20 17:26:03 +0300

    1lab

--- 1lab/1lab.c
+++ 1lab/1lab.c
@@ -5,7 +5,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 void print() {printf("atexit() for pid %d\n",getpid());}
 
 int atexit(void (*function)(void));
@@ -19,22 +18,22 @@ switch(pid=fork()) {
         /* ОШИБОЧКА ВЫШЛА */
         exit(1); /* ПОКА РОДИТЕЛИ */
     case 0:
-        printf(" CHILD: Цей  процесс-дитина!\n");
-        printf(" CHILD: Мiй PID -- %d\n", getpid());
-        printf(" CHILD: PID мого батька -- %d\n", getppid());
-        printf(" CHILD: Введiть мiй код повернення (як можно меньше):");
+        printf(" ДИТИНА: Цей  процесс-дитина!\n");
+        printf(" ДИТИНА: Мiй PID -- %d\n", getpid());
+        printf(" ДИТИНА: PID мого батька -- %d\n", getppid());
+        printf(" ДИТИНА: Введiть мiй код повернення (як можно меньше):");
         scanf("%d",&rv);
-        printf(" CHILD: Вихiд!\n");
+        printf(" ДИТИНА: Вихiд!\n");
         atexit(print);
         break;
     default:
-        printf("PARENT: Цей процесс-батько!\n");
-        printf("PARENT: Мiй PID -- %d\n", getpid());
-        printf("PARENT: PID моэй дитины %d\n",pid);
-        printf("PARENT: Я чекаю, поки дитина не викликаэ exit()...\n");
+        printf("БАТЬКО: Цей процесс-батько!\n");
+        printf("БАТЬКО: Мiй PID -- %d\n", getpid());
+        printf("БАТЬКО: PID моэй дитины %d\n",pid);
+        printf("БАТЬКО: Я чекаю, поки дитина не викликаэ exit()...\n");
         wait(0);
-        printf("PARENT: Код повернення дитины:%d\n", WEXITSTATUS(rv));
-        printf("PARENT: Вихiд!\n");
+        printf("БАТЬКО: Код повернення дитины:%d\n", WEXITSTATUS(rv));
+        printf("БАТЬКО: Вихiд!\n");
         atexit(print);
         exit(rv);
         break;
